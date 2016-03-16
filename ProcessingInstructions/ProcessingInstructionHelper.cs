e48363dc8df1ad43321179c17f02b7adfccbdf59
Jimmy Rishe <jrishe@gmail.com>
2016-03-16 19:07:19 +0900

    Implement GetPseudoAttributes, BuildProcessingInstruction, GetProcessingInstruction

--- ProcessingInstructions/ProcessingInstructionHelper.cs
+++ ProcessingInstructions/ProcessingInstructionHelper.cs
@@ -1,8 +1,63 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 namespace Qdabra.Utility
 {
     public static class ProcessingInstructionHelper
     {
+        public static IDictionary<string, string> GetPseudoAttributes(string processingInstructionValue)
+        {
+            var docXml = "<n " + processingInstructionValue + "/>";
+            try
+            {
+                var doc = XElement.Parse(docXml);
+
+                return doc.Attributes().ToDictionary(a => a.Name.LocalName, a => a.Value);
+            }
+            catch
+            {
+                throw new FormatException("Not a valid pseudo-attribute format: " + processingInstructionValue);
+            }
+        }
+
+        public static IDictionary<string, string> GetPseudoAttributes(XProcessingInstruction pi)
+        {
+            return GetPseudoAttributes(pi.Data);
+        } 
+
+        private static XAttribute TryMakeAttribute(string name, string value)
+        {
+            try
+            {
+                return new XAttribute(name, value ?? "");
+            }
+            catch
+            {
+                throw new FormatException("Invalid pseudo-atttribute name: " + name);
+            }
+        }
+
+        public static string BuildProcessingInstruction(IDictionary<string, string> values)
+        {
+            if (values == null) { throw new ArgumentNullException("values"); }
+
+            var el = new XElement("n", values.Select(kv => TryMakeAttribute(kv.Key, kv.Value)));
+            var elXml = el.ToString();
+
+            return Regex.Replace(elXml, @"^<n|\s*(></n|/)>$", "");
+        }
+
+        public static XProcessingInstruction GetProcessingInstruction(XContainer parent, string name)
+        {
+            if (parent == null) { throw new ArgumentNullException("parent"); }
+            if (string.IsNullOrWhiteSpace(name)) { throw new ArgumentOutOfRangeException("name"); }
+
+            return parent.Nodes()
+                .OfType<XProcessingInstruction>()
+                .FirstOrDefault(pi => string.Equals(pi.Target, name, StringComparison.InvariantCulture));
+        }
     }
 }
--- ProcessingInstructionsTests/ProcessingInstructionHelper.cs
+++ ProcessingInstructionsTests/ProcessingInstructionHelper.cs
@@ -1,9 +1,60 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Linq;
 
-namespace Qdabra.Utilities.Tests
+using PRH = Qdabra.Utility.ProcessingInstructionHelper;
+
+namespace Qdabra.Utility.Tests
 {
     [TestClass]
     public class ProcessingInstructionHelper
     {
+        private XDocument SampleDoc =
+            XDocument.Parse("<?book author=\"J.K. Rowling\" title=\"Harry Potter &amp; the Order of the Phoenix\" genre=\"fiction\"?><book />");
+
+        [TestMethod]
+        public void GetAttributesFromString()
+        {
+            var attrs =
+                PRH.GetPseudoAttributes(
+                    "book=\"Harry Potter &amp; the Philosopher&apos;s Stone\" characters='Harry &amp; Ron &amp; Hermione'");
+
+            Assert.AreEqual(attrs.Count, 2);
+            Assert.AreEqual(attrs["book"], "Harry Potter & the Philosopher's Stone");
+            Assert.AreEqual(attrs["characters"], "Harry & Ron & Hermione");
+        }
+
+        [TestMethod]
+        public void BuildProcessingInstruction()
+        {
+            const string houses = "Gryffindor, Hufflepuff, Slytherin, Ravenclaw";
+            const string pets = "Hedwig, Harry's owl, Scabbers, Ron's rat, Crookshanks, Hermione's cat";
+
+            var pi = PRH.BuildProcessingInstruction(new Dictionary<string, string>
+            {
+                {"houses", houses},
+                {"pets", pets}
+            });
+
+            var attrs = PRH.GetPseudoAttributes(pi);
+
+            Assert.AreEqual(attrs.Count, 2);
+            Assert.AreEqual(attrs["houses"], houses);
+            Assert.AreEqual(attrs["pets"], pets);
+        }
+
+        [TestMethod]
+        public void GetProcessingInstruction()
+        {
+            var pi = PRH.GetProcessingInstruction(SampleDoc, "book");
+            var attrs = PRH.GetPseudoAttributes(pi);
+
+            Assert.AreEqual(attrs.Count, 3);
+            Assert.AreEqual(attrs["title"], "Harry Potter & the Order of the Phoenix");
+
+            var pi2 = PRH.GetProcessingInstruction(SampleDoc, "title");
+            
+            Assert.IsNull(pi2);
+        }
     }
 }
